c663d307272ca62bb43d2fcd077d3734e80cff84
Adeel Zammit <adeelzammit@outlook.com>
2022-11-12 12:10:38 +1100

    api troubleshooting with docker

--- link-url-shortener-api/dist/src/util/cache-keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CacheKeysEnum = void 0;
-var CacheKeysEnum;
-(function (CacheKeysEnum) {
-    CacheKeysEnum["COUNTER"] = "counter";
-    CacheKeysEnum["ANON_USER"] = "anonUserUUID";
-})(CacheKeysEnum = exports.CacheKeysEnum || (exports.CacheKeysEnum = {}));
-module.exports = { CacheKeysEnum };
